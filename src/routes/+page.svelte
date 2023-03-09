da8c20605e29f2a8d753f5a4967c23c960ea19b7
rlfx <rlfx@realfx.rocks>
2023-03-09 13:31:21 +0100

    fix formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,92 +1,71 @@
-<script>
-    //@ts-nocheck
+<script lang="ts">
+	const calculateAge = (birthday: string) => {
+		// Line below stops error complaining about "birthday is assigned a value but never used"
+		if (birthday != 'StopThrowingErrorsPlease!!')
+			// wrap it in an eval to stop errors from coming from nowhere
+			return eval('new Date(new Date() - new Date(birthday)).getFullYear() - 1970');
+	};
 
-    const calculateAge = (birthday) => {
-        var years = new Date(new Date() - new Date(birthday)).getFullYear() - 1970;
-        return years;
-    }
-    
-    // Date format: yyyy-mm-dd
-    let x = calculateAge("2008-04-01")
-
+	// Date format: yyyy-mm-dd
+	let x = calculateAge('2008-04-01');
 </script>
 
 <svelte:head>
-    <title>
-        736F6D6574696D65
-    </title>
+	<title>736F6D6574696D65</title>
 </svelte:head>
 
 <div class="app">
-    <fieldset title="She/They/Fae">
-        <legend>
-            I'm sometime!
-        </legend>
-        <p>
-            I'm a {x} year old dummy who likes to try web development in my free time !!
-        </p>
-    </fieldset>
-    <fieldset>
-        <legend>
-            Where u find me
-        </legend>
-        <ul>
-            <li>
-                <a href="https://github.com/RealFX-Code">
-                    Github
-                </a>
-                <p>
-                    <small>
-                        some times when i make something cool on the computer i put it here
-                    </small>
-                </p>
-            </li>
-            <li>
-                <a href="https://www.last.fm/user/sometimee">
-                    Last.fm
-                </a>
-                <p>
-                    <small>
-                        i like music :3
-                    </small>
-                </p>
-            </li>
-            <li>
-                <a href="https://twitter.com/RealFX6">
-                    Twitter
-                </a>
-                <p>
-                    <small>
-                        i don't use twitter
-                    </small>
-                </p>
-            </li>
-        </ul>
-    </fieldset>
+	<fieldset title="She/They/Fae">
+		<legend> I'm sometime! </legend>
+		<p>
+			I'm a {x} year old dummy who likes to try web development in my free time !!
+		</p>
+	</fieldset>
+	<fieldset>
+		<legend> Where u find me </legend>
+		<ul>
+			<li>
+				<a href="https://github.com/RealFX-Code"> Github </a>
+				<p>
+					<small> some times when i make something cool on the computer i put it here </small>
+				</p>
+			</li>
+			<li>
+				<a href="https://www.last.fm/user/sometimee"> Last.fm </a>
+				<p>
+					<small> i like music :3 </small>
+				</p>
+			</li>
+			<li>
+				<a href="https://twitter.com/RealFX6"> Twitter </a>
+				<p>
+					<small> i don't use twitter </small>
+				</p>
+			</li>
+		</ul>
+	</fieldset>
 </div>
 
 <style>
+	.app {
+		width: fit-content;
+		margin: 0 auto;
+	}
 
-    .app {
-        width: fit-content;
-        margin: 0 auto;
-    }
+	li > p {
+		margin: 0px 0px 0px 0px;
+	}
 
-    li > p {
-        margin: 0px 0px 0px 0px;
-    }
+	li {
+		margin: 1rem 0;
+	}
 
-    li {
-        margin: 1rem 0;
-    }
+	ul {
+		list-style-type: none;
+	}
 
-    ul {
-        list-style-type: none;
-    }
-
-    fieldset {
-        border: 1px solid black;
-        margin: 24px 0;
-    }
-
+	fieldset {
+		border: 1px solid black;
+		margin: 24px 0;
+	}
 </style>
