c098df395a9aed2994784b66258cf62885a86751
leah <rlfx@realfx.rocks>
2024-01-21 20:32:28 +0100

    Add more links to navbar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -7,7 +7,7 @@
         href         : String
     };
 
-    let NavBarLinks : Array<INavBarLink> = [
+    let LeftAlignNavBarLinks : Array<INavBarLink> = [
         {
             friendlyName : "Home",
             href         : "/"
@@ -15,6 +15,17 @@
         {
             friendlyName : "Articles",
             href         : "/articles"
+        }
+    ];
+
+    let RightAlignNavBarLinks : Array<INavBarLink> = [
+        {
+            friendlyName : "Github",
+            href         : "/redirect/github"
+        },
+        {
+            friendlyName : "Discord",
+            href         : "/redirect/discord"
         }
     ];
 
@@ -34,15 +45,28 @@
 
 <div class="navbar">
     <div class="links">
-        {#each NavBarLinks as NavBarLink}
-            <a
-                data-sveltekit-reload
-                class="link {path === NavBarLink.href ? "active" : ""}"
-                href={NavBarLink.href}
-            >
-                {NavBarLink.friendlyName}
-            </a>
-        {/each}
+        <div class="left">
+            {#each LeftAlignNavBarLinks as NavBarLink}
+                <a
+                    data-sveltekit-reload
+                    class="link {path === NavBarLink.href ? "active" : ""}"
+                    href={NavBarLink.href}
+                >
+                    {NavBarLink.friendlyName}
+                </a>
+            {/each}
+        </div>
+        <div class="right">
+            {#each RightAlignNavBarLinks as NavBarLink}
+                <a
+                    data-sveltekit-reload
+                    class="link {path === NavBarLink.href ? "active" : ""}"
+                    href={NavBarLink.href}
+                >
+                    {NavBarLink.friendlyName}
+                </a>
+            {/each}
+        </div>
     </div>
 </div>
 
@@ -67,9 +91,36 @@
         
     }
 
-    div.navbar > div.links > a.link {
+    div.navbar > div.links {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+        grid-auto-columns: 1fr;
+        gap: 0px 0px;
+        grid-auto-flow: row;
+        grid-template-areas: ". .";
+    }
+
+    div.navbar > div.links > div.right {
+        text-align: right;
+    }
+
+    div.navbar > div.links > div > a.link {
         display: inline-block;
         padding: 0px 1rem;
+    }
+
+    @media only screen and (max-width: 600px) {
+        div.navbar > div.links > div.right {
+            display: none;
+        }
+        div.navbar > div.links > div.left {
+            text-align: center;
+        }
+        div.navbar > div.links {
+            grid-template-columns: 1fr;
+            grid-template-areas: ".";
+        }
     }
 
 </style>
---
+++ src/routes/redirect/[link]/+server.ts
@@ -0,0 +1,34 @@
+
+// This somehow works, I'm never changing it.
+
+import { redirect } from '@sveltejs/kit';
+
+interface linkDefinition {
+  urlLink    : string, // E.G. discord
+  redirectTo : string  // E.G. https://discord.gg/feetfinder
+}
+
+const linksDefined : Array<linkDefinition> = [
+  {
+    urlLink    : "discord",
+    redirectTo : "https://discord.gg/qFdvxqsRg5"
+  },
+  {
+    urlLink    : "github",
+    redirectTo : "https://github.com/RealFX-Code"
+  }
+]
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({params}) {
+
+  let redirectURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+  linksDefined.forEach(function(value) {
+    if (params.link == value.urlLink) {
+      redirectURL = value.redirectTo;
+    }
+  });
+
+  throw redirect(302, redirectURL)
+}
