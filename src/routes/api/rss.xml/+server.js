a868e5c247a88f41cf6cb620c7a944c2fa7d10fb
Leah <rlfx@realfx.rocks>
2024-03-02 16:54:58 +0100

    fix RSS stuffs

--- src/routes/api/rss.xml/+server.js
+++ src/routes/api/rss.xml/+server.js
@@ -27,13 +27,13 @@ const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
 <title>${siteTitle}</title>
 <description>${siteDescription}</description>
 <link>${siteURL}</link>
-<atom:link href="${siteURL}/rss.xml" rel="self" type="application/rss+xml"/>
+<atom:link href="${siteURL}/api/rss.xml" rel="self" type="application/rss+xml"/>
 ${posts
 	.map(
 		(post) => `<item>
-<guid isPermaLink="true">${siteURL}/blog/${post.path}</guid>
+<guid isPermaLink="true">${siteURL + post.path}</guid>
 <title>${post.meta.title}</title>
-<link>${siteURL}/blog/${post.path}</link>
+<link>${siteURL + post.path}</link>
 <description>${post.meta.title}</description>
 <pubDate>${new Date(post.meta.date).toUTCString()}</pubDate>
 </item>`
