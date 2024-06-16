684a9818f6a6f32a4692b225b3ea85fc6048dab6
leah <rlfx@realfx.rocks>
2024-06-16 16:37:26 +0200

    Add placeholder content to index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
     import NameBlinker from '$lib/components/NameBlinker.svelte';
-    import { Heading, P, Card } from 'flowbite-svelte';
+    import { Heading, P, A, Card } from 'flowbite-svelte';
+
+	const calculateAge = (birthday: Date) => {
+        //@ts-expect-error I don't want to argue with typescript anymore.
+		return new Date(new Date() - new Date(birthday)).getFullYear() - 1970;
+	};
+
+	// Date format: yyyy-mm-dd ( Add one day bcs calculation is slightly off. )
+	let age = calculateAge(new Date('2008-04-02'));
+
 </script>
 
 <!-- Heading -->
@@ -11,16 +20,15 @@
     </Heading>
     <hr class="my-6 border-gray-500 dark:border-primary-600 sm:mx-auto lg:my-8" />
     <P>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque exercitationem
-        quaerat reiciendis quo numquam enim dicta et quis minus officiis, optio explicabo dolorum
-        tempora, atque in laudantium. Itaque, vel!
+        Hi, you've seen my name above so I'll skip that part. I'm a {age} year old girl who's into technology stuff.<br>
+        I'm autistic, and technology (anything computers) is my hyperfixation, so I spend all of my time on it.
+        Mainly I like to play with obscure computers, and make devices to stuff they weren't intended to, like the obligatory neofetch!<br>
+        If you're against LGBTQ+ in any way, GTFO. I'm transgender and strongly want to fight for transgender rights.
     </P>
     <br />
     <Card class="w-max max-w-fit bg-primary-50 dark:border-primary-700 dark:bg-primary-800">
         <P>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, eos totam numquam
-            adipisci facere omnis aspernatur quod tempore maxime corrupti quaerat earum doloremque?
-            Corrupti quibusdam sequi deleniti assumenda laboriosam dignissimos?
+            Want to know how to address me properly? Check out: <A target="_blank" href="https://prns.realfx.rocks/@sometime">prns.realfx.rocks/@sometime</A>
         </P>
     </Card>
 </section>
