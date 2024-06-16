a462c3b30d54f721632f9a616d2bcf82fe62267b
leah <rlfx@realfx.rocks>
2024-06-16 16:07:39 +0200

    Fix name blinking on non-chromium browsers.

--- src/lib/components/NameBlinker.svelte
+++ src/lib/components/NameBlinker.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import { cubicInOut } from 'svelte/easing';
 
     import { fade } from 'svelte/transition';
 
+    // The first item is moved to the end of the list, due to some wierd stuff, idk.
     const names: Array<string> = ['Sometime', 'RealFX', '[deadname]', 'Leah'];
 
     const interval: number = 1500;
@@ -18,16 +20,36 @@
     });
 
     const easingDuration = interval / 4;
+
+    let isChromium : boolean;
+    onMount(function(){
+        //@ts-expect-error Window isn't defined on the server and thus throws an LSP error.
+        isChromium = !!window.chrome;
+
+        if (!isChromium){
+            console.warn("Non-chromium browser detected, Name blinker is disabled.");
+        }
+
+    })
+
 </script>
 
-{#key currentName}
-    <div class="block">
-        <span
-            class="absolute"
-            in:fade={{ duration: easingDuration, easing: cubicInOut, delay: easingDuration }}
-            out:fade={{ duration: easingDuration, easing: cubicInOut }}
-        >
-            {currentName}
+{#if isChromium}
+    {#key currentName}
+        <div class="block">
+            <span
+                class="absolute"
+                in:fade={{ duration: easingDuration, easing: cubicInOut, delay: easingDuration }}
+                out:fade={{ duration: easingDuration, easing: cubicInOut }}
+            >
+                {currentName}
+            </span>
+        </div>
+    {/key}
+{:else}
+    <div class="block" id="non-chromium">
+        <span class="absolute">
+            {names[names.length - 1]}
         </span>
     </div>
-{/key}
+{/if}
