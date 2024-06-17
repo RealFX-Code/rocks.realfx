7f3010c844e14e75e1064115472eaecd41068b57
leah <rlfx@realfx.rocks>
2024-06-17 21:41:01 +0200

    OpenGraph stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,9 +38,38 @@
             href: '/contact'
         }
     ];
+
+    interface iMeta {
+        title: string,
+        description: string,
+        image: string
+    }
+
+    const meta : iMeta = {
+        title: "rocks.realfx",
+        description: "Hi, I'm Leah! I like to do tech stuff. My website does contain some stuff I've done, so be sure to check it out if you're into tech.",
+        image: "https://www.realfx.rocks/media/image-001.webp"
+    }
+
 </script>
 
 <svelte:head>
+    <title>{meta.title}</title>
+    <meta name="description" content="{meta.description}">
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:url" content="http://rocks.realfx">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{meta.title}">
+    <meta property="og:description" content="{meta.description}">
+    <meta property="og:image" content="{meta.image}">
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="rocks.realfx">
+    <meta property="twitter:url" content="http://rocks.realfx">
+    <meta name="twitter:title" content="{meta.title}">
+    <meta name="twitter:description" content="{meta.description}">
+    <meta name="twitter:image" content="{meta.image}">
+
     <script
         defer
         src="https://eu.umami.is/script.js"
@@ -94,6 +123,9 @@
                 <FooterLink href="https://github.com/RealFX-Code/rocks.realfx"
                     >Website Repo</FooterLink
                 >
+                <FooterLink href="https://unsplash.com/photos/a-black-and-white-photo-of-a-curved-object-16KAzCV7UVI">
+                    OpenGraph image
+                </FooterLink>
             </FooterLinkGroup>
         </div>
         <hr class="my-6 border-gray-200 dark:border-primary-600 sm:mx-auto lg:my-8" />
