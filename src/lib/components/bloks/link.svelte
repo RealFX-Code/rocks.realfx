c1be102ae05da2af9dd04a66422844201202eaf9
leah <rlfx@realfx.rocks>
2024-06-19 22:40:34 +0200

    make Link element use typescript since I was stupid

--- src/lib/components/bloks/link.svelte
+++ src/lib/components/bloks/link.svelte
@@ -1,30 +1,21 @@
-<script>
+<script lang="ts">
     import { storyblokEditable } from '@storyblok/svelte';
 
     import { A, P } from 'flowbite-svelte';
     import { onMount } from 'svelte';
 
-    /**
-     * @type {import("@storyblok/svelte").SbBlokData}
-     */
     export let blok;
 
-    /**
-     * @type {string}
-     */
-    let url;
-
-    /**
-     * @type {string}
-     */
-    let title;
+    let url : string;
+    let title : string;
 
     onMount(function () {
-        // @ts-ignore
-        url = blok?.link?.url;
 
-        // @ts-ignore
+        // These do exist, but since they're dynamically added to "blok" they have no intellisense afaik.
+        // If you'd like to know what is in these just add a "console.log(blok);" after "blok" is updated and you'll find out.
+        url = blok?.link?.url;
         title = blok?.title;
+
     });
 </script>
 
