0912942f55529cd7248d525f799681e4f8e77303
Leah <leah@Mac-mini.localdomain>
2024-06-26 16:39:22 +0200

    Add resume :3

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { StoryblokComponent, useStoryblokBridge } from '@storyblok/svelte';
+
+    export let data;
+
+    import { onMount } from 'svelte';
+
+
+    onMount(function(){
+        if(data.story){
+            useStoryblokBridge(data.story.id, (newStory) => (data.story = newStory));
+        }
+    });
+
+</script>
+
+<section class="mx-auto w-[calc(100%-10%)] sm:w-[calc(100%-35%)]">
+    <div>
+        {#if data.story}
+            <StoryblokComponent blok={data.story.content} />
+        {/if}
+    </div>
+</section>
---
+++ src/routes/resume/+page.ts
@@ -0,0 +1,11 @@
+
+export async function load({ params, parent }) {
+    const { storyblokApi } = await parent();
+    let path = 'cdn/stories/resume';
+    const dataStory = await storyblokApi.get(path, {
+        version: 'draft'
+    });
+    return {
+        story: dataStory.data.story
+    };
+}
